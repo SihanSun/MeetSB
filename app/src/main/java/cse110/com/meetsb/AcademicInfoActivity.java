50c58ee76a9e81c4d59cdccb2e798f4aa614dbd8
Zhongyu Chen <zhc180@ucsd.edu>
2018-10-24 20:20:18 -0700

    Added class activity and link the three activities

--- app/src/main/java/cse110/com/meetsb/AcademicInfoActivity.java
+++ app/src/main/java/cse110/com/meetsb/AcademicInfoActivity.java
@@ -1,13 +1,26 @@
 package cse110.com.meetsb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AcademicInfoActivity extends AppCompatActivity {
-
+    Button continueBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_academic_info);
+        continueBtn = (Button) findViewById(R.id.academic_info_button_continue);
+
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO::
+                finish();
+                startActivity(new Intent(AcademicInfoActivity.this, ClassInfoActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/cse110/com/meetsb/BasicInfoActivity.java
+++ app/src/main/java/cse110/com/meetsb/BasicInfoActivity.java
@@ -1,13 +1,26 @@
 package cse110.com.meetsb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BasicInfoActivity extends AppCompatActivity {
-
+    Button continueBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_info);
+
+        continueBtn = (Button) findViewById(R.id.basic_info_button_continue);
+        continueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO::
+                finish();
+                startActivity(new Intent(BasicInfoActivity.this, AcademicInfoActivity.class));
+            }
+        });
     }
 }
