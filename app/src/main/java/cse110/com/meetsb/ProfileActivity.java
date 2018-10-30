8163fa0e238ec0c9dfb3ce528226cc0339b6482d
Paige <paigexu2016@gmail.com>
2018-10-30 16:53:13 -0700

    update profile info

--- app/src/main/java/cse110/com/meetsb/ProfileActivity.java
+++ app/src/main/java/cse110/com/meetsb/ProfileActivity.java
@@ -5,7 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import cse110.com.meetsb.Model.User;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +22,20 @@ public class ProfileActivity extends AppCompatActivity {
 
     public void openSetting(View view) {
         Intent intent = new Intent(this, SettingActivity.class);
+        startActivity(intent);
+    }
+
+    private void getPersonalInfo() {
+        User newUser = new User();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        String userId = firebaseAuth.getCurrentUser().getUid();
+        databaseReference.child("USERTABLE").child(userId);
+
+        Intent intent = new Intent(this, SwipeActivity.class);
+        finish();
         startActivity(intent);
     }
 }
