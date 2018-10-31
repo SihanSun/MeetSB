786d860d41ee1bc5d8c1c146e4c81b1d2f6355fe
Xinpei Tan <t347100955@gmail.com>
2018-10-31 14:21:12 -0700

    Added more comments and description.

--- app/src/main/java/cse110/com/meetsb/SwipeActivity.java
+++ app/src/main/java/cse110/com/meetsb/SwipeActivity.java
@@ -19,32 +19,32 @@ public class SwipeActivity extends AppCompatActivity {
     private StudentCardAdapter arrayAdapter;
     private SwipeFlingAdapterView flingContainer;
     private int i;
-    private List<List<String>> list;
+    private List<List<String>> imageList;
     private ImageView btnDislike, btnLike, btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_swipe);
 
-        list = new ArrayList<>();
+        imageList = new ArrayList<>();
         for (int i = 0; i < imageUrls.length; i++) {
             List<String> s = new ArrayList<>();
             s.add(imageUrls[i]);
-            list.add(s);
+            imageList.add(s);
         }
 
 
         al = new ArrayList<>();
-        Student gary = new Student("Gary", 21, list.get(1));
+        Student gary = new Student("Gary", 21, imageList.get(1));
         al.add(new StudentCardMode( gary ));
-        al.add(new StudentCardMode("Still Gary", 21, list.get(1)));
-        al.add(new StudentCardMode("Another Gary", 21, list.get(1)));
-        al.add(new StudentCardMode("Gary Again", 21, list.get(1)));
-        al.add(new StudentCardMode("Double Gary", 21, list.get(1)));
-        al.add(new StudentCardMode("Professor Gary", 21, list.get(1)));
-        al.add(new StudentCardMode("Gary Gary", 21, list.get(1)));
-        al.add(new StudentCardMode("Gary 教授", 21, list.get(1)));
+        al.add(new StudentCardMode("Still Gary", 21, imageList.get(1)));
+        al.add(new StudentCardMode("Another Gary", 21, imageList.get(1)));
+        al.add(new StudentCardMode("Gary Again", 21, imageList.get(1)));
+        al.add(new StudentCardMode("Double Gary", 21, imageList.get(1)));
+        al.add(new StudentCardMode("Professor Gary", 21, imageList.get(1)));
+        al.add(new StudentCardMode("Gary Gary", 21, imageList.get(1)));
+        al.add(new StudentCardMode("Gary 教授", 21, imageList.get(1)));
 
         arrayAdapter = new StudentCardAdapter(this, R.layout.item, R.id.buddyName, al);
 
@@ -69,13 +69,17 @@ public class SwipeActivity extends AppCompatActivity {
 
             @Override
             public void onRightCardExit(Object dataObject) {
+                //Do something on the right!
                 makeToast(SwipeActivity.this, "You made one of the best decision in your lifetime ;)");
             }
 
             @Override
+            /**
+             * what to do when list is about to be empty
+             */
             public void onAdapterAboutToEmpty(int itemsInAdapter) {
                 // Ask for more data here
-                al.add(new StudentCardMode("No More Gary", 21, list.get(itemsInAdapter % imageUrls.length - 1)));
+                al.add(new StudentCardMode("No More Gary", 21, imageList.get(itemsInAdapter % imageUrls.length - 1)));
                 arrayAdapter.notifyDataSetChanged();
                 Log.d("LIST", "notified");
                 i++;
@@ -95,7 +99,7 @@ public class SwipeActivity extends AppCompatActivity {
         });
 
 
-        // Optionally add an OnItemClickListener
+        // add an OnItemClickListener to define what to do after item being clicked
         flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
             @Override
             public void onItemClicked(int itemPosition, Object dataObject) {
@@ -106,6 +110,7 @@ public class SwipeActivity extends AppCompatActivity {
         btnDislike = (ImageView) findViewById(R.id.left);
         btnLike = (ImageView) findViewById(R.id.right);
         btnProfile  = (ImageView) findViewById(R.id.info);
+
         btnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
