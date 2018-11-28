f67812bb64196cd365b195938b19ff411bc1d391
Paige <paigexu2016@gmail.com>
2018-11-28 14:11:58 -0800

    Clicking the addCourse button can jump to another activity

--- app/src/main/java/cse110/com/meetsb/AddClassActivity.java
+++ app/src/main/java/cse110/com/meetsb/AddClassActivity.java
@@ -1,12 +1,30 @@
 package cse110.com.meetsb;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import cse110.com.meetsb.Model.User;
 
 public class AddClassActivity extends AppCompatActivity {
 
@@ -15,6 +33,8 @@ public class AddClassActivity extends AppCompatActivity {
     private FirebaseDatabase databaseInstance;
     private DatabaseReference databaseRef;
     FirebaseAuth auth;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> selectClass;
 
 
 
@@ -23,11 +43,50 @@ public class AddClassActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_info);
 
-        auth = FirebaseAuth.getInstance();
-        databaseInstance = FirebaseDatabase.getInstance();
-        databaseRef = databaseInstance.getReference().child("USER");
+
+        //set list view
+        final ListView lv = (ListView)findViewById(R.id.class_info_listview_classlist);
+        final ArrayList<String> arrayClass = new ArrayList<>();
+        arrayClass.addAll(Arrays.asList(getResources().getStringArray(R.array.class_array)));
+        selectClass = new ArrayList<String>();
+        adapter = new ArrayAdapter<>(AddClassActivity.this,
+                android.R.layout.simple_list_item_1,
+                arrayClass);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //courseTaking.add(view.toString());
+                //TODO
+                //add the existing classes to the view
+                String item = (lv.getItemAtPosition(i)).toString();
+                if (!selectClass.contains(item)) {
+                    selectClass.add(item);
+                    ListView sl = (ListView)findViewById(R.id.class_info_listview_addedClass);
+                    ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(),
+                            android.R.layout.simple_list_item_1, selectClass);
+                    sl.setAdapter(adapter);
+                }
+            }
+        });
+
+
 
         submit = findViewById(R.id.class_info_button_submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+                //Save infomation in the fire base
+
+            }
+        });
+
+        setContentView(R.layout.activity_class_info);
 
     }
+
+
+
 }
